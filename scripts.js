49009471b2e9811d22ec9ee252c5887b1e1a25f3
Thestickman391 <stickman391@gmail.com>
2021-01-08 18:28:04 +1300

    Added two new modes and alternate AA EE OO's

--- scripts.js
+++ scripts.js
@@ -1,54 +1,91 @@
 function playSound(soundSrc) {
-	var cloned = $(soundSrc).clone()[0];
+	if (document.getElementById("overlap").checked) {
+		document.getElementById(soundSrc).pause();
+		document.getElementById(soundSrc).currentTime = 0;
+		document.getElementById(soundSrc).play();
+	}
+	else {
+		var cloned = $("#" + soundSrc).clone()[0];
 
-    cloned.play();
+		cloned.play();
 
-    cloned.onended = function() {
-        $(cloned).remove();
-    }
+		cloned.onended = function() {
+			$(cloned).remove();
+		}
+	}
 }
 
 document.addEventListener('keydown', function(e) {
-  if (e.keyCode == 65) {
-    playSound("#aa");
+  if (e.keyCode == 65){
+	if (document.getElementById("alternate").checked) {
+		playSound("aa_alt");
+	}
+	else {
+		playSound("aa");
+	}  
   }
 });
 
 document.addEventListener('keydown', function(e) {
   if (e.keyCode == 69) {
-    playSound("#ee");
+    if (document.getElementById("alternate").checked) {
+		playSound("ee_alt");
+	}
+	else {
+		playSound("ee");
+	}  
   }
 });
 
 document.addEventListener('keydown', function(e) {
   if (e.keyCode == 79) {
-    playSound("#oo");
+    if (document.getElementById("alternate").checked) {
+		playSound("oo_alt");
+	}
+	else {
+		playSound("oo");
+	}  
   }
 });
 
 document.addEventListener('keydown', function(e) {
   if (e.keyCode == 32) {
-    playSound("#aj");
+    playSound("aj");
   }
 });
 
 // "Nice, ron"
 document.addEventListener('keydown', function(e) {
   if (e.keyCode == 78) {
-    playSound("#nr");
+    playSound("nr");
   }
 });
 
 // sneeze
 document.addEventListener('keydown', function(e) {
   if (e.keyCode == 83) {
-    playSound("#sn");
+    playSound("sn");
   }
 });
 
 // "What, I sneezed, I'm not allowed to sneeze?"
 document.addEventListener('keydown', function(e) {
   if (e.keyCode == 87) {
-    playSound("#ws");
+    playSound("ws");
   }
 });
+
+function show_alternate() {
+	document.getElementById("options-help-alternate").style.display = "flex";
+}
+
+function hide_alternate() {
+	document.getElementById("options-help-alternate").style.display = "none";
+}
+function show_overlap() {
+	document.getElementById("options-help-overlap").style.display = "flex";
+}
+
+function hide_overlap() {
+	document.getElementById("options-help-overlap").style.display = "none";
+}
